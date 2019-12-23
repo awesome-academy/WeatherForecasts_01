114550d41910459e19f835c9c30599b4c1fbbe44
Thai Tuan Hiep (FPT Aptech) <hiepttd00739@fpt.edu.vn>
2019-12-23 18:06:40 +0700

    create splash screen

---
+++ app/src/main/java/com/habit/weatherforecasts_01/screen/splash/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.habit.weatherforecasts_01.screen.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.habit.weatherforecasts_01.R;
+import com.habit.weatherforecasts_01.screen.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final long SPLASH_TIME_DELAY = 1500;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_DELAY);
+    }
+}
